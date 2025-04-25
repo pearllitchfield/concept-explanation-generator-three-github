dc00650df2abeb6a34d023c7aba97278abecc58a
pearllitchfield <ptlitchfield@icloud.com>
2025-04-25 02:40:05 +0200

    Updated script.js
    Added some javascript to script.js file.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function generateConcept(event) {
   event.preventDefault();
 
-  let searchResultElement = document.querySelector("#search-result");
-  searchResultElement.innerHTML =
-    "A singularity realm refers to a state or concept that is often used in both scientific and fictional contexts. In physics, a singularity is a point of infinite density and gravity, like the center of a black hole. In the realm of AI, the term refers to a potential future point where artificial intelligence surpasses human intelligence.";
+  new Typewriter("#search-result", {
+    strings:
+      "A singularity realm refers to a state or concept that is often used in both scientific and fictional contexts. In physics, a singularity is a point of infinite density and gravity, like the center of a black hole. In the realm of AI, the term refers to a potential future point where artificial intelligence surpasses human intelligence.",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
 }
 
-let conceptFormElement = document.querySelector("concept-generator-form");
+let conceptFormElement = document.querySelector("#concept-generator-form");
 conceptFormElement.addEventListener("submit", generateConcept);
