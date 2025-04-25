b222e3ace0e3669b0c5958b040af7033decaa75f
pearllitchfield <ptlitchfield@icloud.com>
2025-04-25 16:34:19 +0200

    Updated script.js
    Added axios functionality to script.js file.

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ function displayConcept(response) {
 function generateConcept(event) {
   event.preventDefault();
 
+  let searchBarInput = document.querySelector("#user-input");
   let apiKey = "7da7782c88c36f93atbb1b04a1aebo6b";
-  let context = "";
-  let prompt = "";
+  let context =
+    "You are an expert and have a great deal of knowledge on a blend of scientific concepts. Your answers are restricted to four lines of explanation only, follow user instructions explicitly. Sign and bold with 'SheCodes AI'";
+  let prompt = `User instructions: Generate a concept about ${searchBarInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayConcept);
